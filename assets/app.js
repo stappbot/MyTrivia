5842c830e6adf24aed12401508f24ccde7025cef
J. Silverthorn <jnsilverthorn@ucdavis.edu>
2019-10-31 18:39:23 -0700

    added firebase info to js

---
+++ assets/app.js
@@ -0,0 +1,11 @@
+var config = {
+    apiKey: "AIzaSyC9pNO4Wn-cDkXf1iM721Bi5cOKfWprogE",
+    authDomain: "project-one-41a36.firebaseapp.com",
+    databaseURL: "https://project-one-41a36.firebaseio.com",
+    projectId: "project-one-41a36",
+    storageBucket: "project-one-41a36.appspot.com",
+    messagingSenderId: "768894251614",
+    appId: "1:768894251614:web:3a6383dbd6e22636d4facf"
+}
+
+firebase.initializeApp(config);
